5bd2a2a8c736505ffea45db8891ec5d756a484f4
Erik Larkö <erik@larko.se>
2023-10-15 18:56:57 -0400

    Rewrite reminderBuilder test since grouping is no longer its responsibility. The test fails because the dueDate is using `DateTime.now`

--- lib/models/reminder.dart
+++ lib/models/reminder.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart' hide Action;
 import 'package:poke/models/action.dart';
+import 'package:poke/utils/date_formatter.dart';
 
 class Reminder {
   final Action action;
@@ -9,5 +10,24 @@ class Reminder {
 
   Widget buildReminderListItem(BuildContext context) {
     return action.buildReminderListItem(context);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (other is! Reminder || other.runtimeType != runtimeType) {
+      return false;
+    }
+
+    return other.action == action && other.dueDate == dueDate;
+  }
+
+  @override
+  int get hashCode {
+    return action.hashCode + dueDate.hashCode;
+  }
+
+  @override
+  String toString() {
+    return "$action due $dueDate";
   }
 }
--- test/event_storage/reminder_builder_test.dart
+++ test/event_storage/reminder_builder_test.dart
@@ -1,49 +1,24 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:poke/event_storage/in_memory_storage.dart';
 import 'package:poke/event_storage/reminder_builder.dart';
-import 'package:poke/models/watering_plants/plant.dart';
-import 'package:poke/models/watering_plants/water_plant.dart';
-
-import '../utils/clock.dart';
-
-final plant1 = Plant(name: 'Plant 1');
-final plant2 = Plant(name: 'Plant 2');
-
-final clock = Clock();
+import 'package:poke/models/reminder.dart';
+import 'package:poke/models/test-action/test_action.dart';
 
 void main() {
-  test('Groups plant events based on which plant was watered', () async {
+  test('reminds about existing events', () async {
     final eventStorage = InMemoryStorage();
 
-    // create two different actions to be logged at different timestamps further
-    // down
-    final a1 = WaterPlantAction(
-      plant: plant1,
-      addedFertilizer: false,
-    );
-    final a2 = WaterPlantAction(
-      plant: plant2,
-      addedFertilizer: false,
-    );
+    final action = TestAction(id: '1');
+    await eventStorage.logAction(action, DateTime.now());
 
-    // get references to the timestamps we're logging the actions at so that we
-    // can check them later
-    final ts1 = clock.next();
-    final ts2 = clock.next();
-    final ts3 = clock.next();
-
-    // log the actions at their corresponding timestamps
-    await eventStorage.logAction(a1, ts1);
-    await eventStorage.logAction(a2, ts2);
-    await eventStorage.logAction(a1, ts3);
-
-    final expected = {
-      a1: [ts1, ts3],
-      a2: [ts2],
-    };
     expect(
-      await eventStorage.getAll(),
-      equals(expected),
+      await buildReminders(eventStorage),
+      equals([
+        Reminder(
+          action: action,
+          dueDate: DateTime.now(),
+        ),
+      ]),
     );
   });
 }
