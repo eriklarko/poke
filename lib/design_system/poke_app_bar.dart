3e8cff412f47f8a13caf38795fd0551095c8bedc
Erik Larkö <erik@larko.se>
2023-10-17 22:06:05 -0400

    Add link to settings in appbar

--- lib/design_system/poke_app_bar.dart
+++ lib/design_system/poke_app_bar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:poke/design_system/poke_text.dart';
+import 'package:poke/screens/settings_screen.dart';
+import 'package:poke/utils/nav_service.dart';
 
 class PokeAppBar extends AppBar {
   PokeAppBar(BuildContext context, {super.key, String title = 'Poke 👉'})
@@ -10,5 +12,11 @@ class PokeAppBar extends AppBar {
           // Stop the app bar turning darker while scrolling
           // https://stackoverflow.com/a/72773421
           scrolledUnderElevation: 0,
+
+          actions: [
+            IconButton(
+                onPressed: () => NavService.push(const SettingsScreen()),
+                icon: const Icon(Icons.settings)),
+          ],
         );
 }
