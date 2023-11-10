c5075da428bd434c4fab13803b77d787a903bcc0
Erik Larkö <erik@larko.se>
2023-11-10 11:12:10 -0500

    Wrap MaterialApp in AppLifecycleListener

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:poke/components/app_lifecycle_listener.dart';
 import 'package:poke/screens/loading/initialize_app.dart';
 import 'package:poke/screens/loading/loading_screen.dart';
 import 'package:poke/utils/nav_service.dart';
@@ -15,25 +16,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
-      ),
+    return AppLifecycleListener(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+          useMaterial3: true,
+        ),
 
-      // Register the navKey belonging to the global navigator
-      navigatorKey: NavService.internal.key,
+        // Register the navKey belonging to the global navigator
+        navigatorKey: NavService.internal.key,
 
-      // Render the LoadingScreen. It will take care of loading any resources
-      // needed by the rest of the app, such as Firebase registration and
-      // reading images from disk.
-      //
-      // There's a Firebase.auth listener registered as part of initalization
-      // that will kick in and navigate to the login or home screens as
-      // appropriate.
-      home: LoadingScreen(
-        loadingFuture: initializeApp(),
+        // Render the LoadingScreen. It will take care of loading any resources
+        // needed by the rest of the app, such as Firebase registration and
+        // reading images from disk.
+        //
+        // There's a Firebase.auth listener registered as part of initalization
+        // that will kick in and navigate to the login or home screens as
+        // appropriate.
+        home: LoadingScreen(
+          loadingFuture: initializeApp(),
+        ),
       ),
     );
   }
