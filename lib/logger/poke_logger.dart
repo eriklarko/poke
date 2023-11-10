a7791f71b4a63de64b768af081c0a97c36936492
Erik Larkö <erik@larko.se>
2023-11-10 12:21:32 -0500

    Improve logging stack trace

--- lib/logger/poke_logger.dart
+++ lib/logger/poke_logger.dart
@@ -36,7 +36,13 @@ abstract interface class PokeLogger {
 
 class FirebaseLogger implements PokeLogger {
   final PokeFirebase firebase;
-  final Logger _logger = Logger();
+  final Logger _logger = Logger(
+    printer: PrettyPrinter(
+      // Skip the frames coming from this logger
+      stackTraceBeginIndex: 3,
+      methodCount: 5,
+    ),
+  );
 
   FirebaseLogger(this.firebase);
 
@@ -47,22 +53,22 @@ class FirebaseLogger implements PokeLogger {
 
   @override
   Future trace(String msg, {Map<String, dynamic>? data}) {
-    return log(Level.trace, msg, data: data);
+    return _log(Level.trace, msg, data: data);
   }
 
   @override
   Future debug(String msg, {Map<String, dynamic>? data}) {
-    return log(Level.debug, msg, data: data);
+    return _log(Level.debug, msg, data: data);
   }
 
   @override
   Future info(String msg, {Map<String, dynamic>? data}) {
-    return log(Level.info, msg, data: data);
+    return _log(Level.info, msg, data: data);
   }
 
   @override
   Future warn(String msg, {Map<String, dynamic>? data}) {
-    return log(Level.warning, msg, data: data);
+    return _log(Level.warning, msg, data: data);
   }
 
   @override
@@ -72,7 +78,7 @@ class FirebaseLogger implements PokeLogger {
     Object? error,
     StackTrace? stackTrace,
   }) {
-    return log(Level.error, msg,
+    return _log(Level.error, msg,
         data: data, error: error, stackTrace: stackTrace);
   }
 
@@ -83,12 +89,32 @@ class FirebaseLogger implements PokeLogger {
     Object? error,
     StackTrace? stackTrace,
   }) {
-    return log(Level.fatal, msg,
+    return _log(Level.fatal, msg,
         data: data, error: error, stackTrace: stackTrace);
   }
 
   @override
   Future log(
+    Level level,
+    String msg, {
+    Map<String, dynamic>? data,
+    Object? error,
+    StackTrace? stackTrace,
+  }) {
+    // We configure the log printer to skip the first three frames in the stack
+    // trace to make the `Logger.LEVEL(...)` calls show a more usable trace. To
+    // not skip important frames if calling `Logger.log(Level...)` we must call
+    // another method here.
+    return _log(
+      level,
+      msg,
+      data: data,
+      error: error,
+      stackTrace: stackTrace,
+    );
+  }
+
+  Future _log(
     Level level,
     String msg, {
     Map<String, dynamic>? data,
@@ -123,7 +149,13 @@ class FirebaseLogger implements PokeLogger {
       data['__msg'] = msg;
       logMessage = data;
     }
-    _logger.log(level, logMessage, error: error, stackTrace: stackTrace);
+
+    _logger.log(
+      level,
+      logMessage,
+      error: error,
+      stackTrace: stackTrace ?? StackTrace.current,
+    );
   }
 
   // ignores trace, debug and info
