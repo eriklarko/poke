e1e85894c652b086e3bf89afd98358b82957a6e6
Erik Larko <erik.larko@gmail.com>
2024-11-27 21:57:15 -0500

    Update reminder list item to use icons instead of text

--- lib/models/watering_plants/widgets/reminder_list_item.dart
+++ lib/models/watering_plants/widgets/reminder_list_item.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:poke/design_system/poke_constants.dart';
 import 'package:poke/design_system/poke_text.dart';
 import 'package:poke/design_system/poke_time_ago.dart';
@@ -28,31 +28,59 @@ class PlantReminderListItem extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               PokeText(plant.name),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  if (lastEvent != null)
-                    PokeTimeAgo(
-                      key: ValueKey('last-watered-${plant.id}'),
-                      date: lastEvent.$1,
-                      format: (timeAgo) => "Last watered $timeAgo",
-                    ),
-                  if (lastEvent?.$2!.addedFertilizer == true)
-                    const PokeFinePrint('included fertilizer'),
-                  if (reminder.dueDate != null)
-                    PokeTimeAgo(
-                      key: ValueKey('due-${plant.id}'),
-                      date: reminder.dueDate!,
-                      format: (timeAgo) => reminder.isDue()
-                          ? "Due $timeAgo"
-                          : "Will poke $timeAgo",
-                    ),
-                ],
+              PokeConstants.FixedSpacer(),
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (lastEvent != null)
+                      _renderIconAndText(
+                        ValueKey('last-watered-${plant.id}'),
+                        Icons.water_drop_outlined,
+                        lastEvent.$1,
+                      ),
+                    Expanded(child: Container()),
+                    if (reminder.dueDate != null)
+                      Padding(
+                        padding: EdgeInsets.only(right: PokeConstants.space(2)),
+                        child: _renderIconAndText(
+                          ValueKey('due-${plant.id}'),
+                          Icons.alarm_outlined,
+                          reminder.dueDate!,
+                        ),
+                      ),
+                  ],
+                ),
               ),
             ],
           ),
         ),
       ],
     );
+  }
+
+  Widget _renderIconAndText(Key key, IconData icon, DateTime eventDate) {
+    return Row(
+      children: [
+        Icon(icon, color: PokeConstants.colors.primary),
+        PokeConstants.FixedSpacer(),
+        ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 75),
+          child: PokeTimeAgo(
+            key: key,
+            date: eventDate,
+            format: formatTimeAgo,
+          ),
+        ),
+      ],
+    );
+  }
+
+  // this is the most beautiful function I've ever written
+  String formatTimeAgo(String timeAgo) {
+    if (timeAgo.contains('from now')) {
+      return "In ${timeAgo.replaceAll('from now', '')}";
+    }
+    return timeAgo;
   }
 }
